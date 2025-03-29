c19daea33e1c42e7bb18f1fae59c82cdca987beb
taaanmoy-git <taaanmoy.saha@gmail.com>
2025-03-30 02:14:23 +0530

    Added html Dependency

---
+++ src/main/java/com/jwt/d01/jwtscd01/Jwtscd01Application.java
@@ -0,0 +1,13 @@
+package com.jwt.d01.jwtscd01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtscd01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwtscd01Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/jwt/d01/jwtscd01/service/UserService.java
@@ -0,0 +1,23 @@
+package com.jwt.d01.jwtscd01.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.jwt.d01.jwtscd01.entity.User;
+
+@Service
+public class UserService {
+
+    private List<User> store = new ArrayList<>();
+
+    public UserService() {
+        store.add(new User("U001", "Alice", "alice@example.com"));
+        store.add(new User("U002", "Bob", "bob@example.com"));
+        store.add(new User("U003", "Charlie", "charlie@example.com"));
+        store.add(new User("U004", "Diana", "diana@example.com"));
+    }
+
+    public List<User> getAllUsers() {
+        return this.store;
+    }
+}
